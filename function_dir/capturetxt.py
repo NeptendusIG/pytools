002f6c1e6f3f3fcde1698e78889c1b96cd06880a
Gtlp001 <gaetan.lepage18@protonmail.com>
2024-10-21 09:42:57 +0200

    feat: tool capture text (read print screen and paste text in clipboard)

---
+++ function_dir/capturetxt.py
@@ -0,0 +1,71 @@
+# ----------------------------
+#      Capture Text Tool 
+# ----------------------------
+# Retrive text from a screenshot to clipborad
+
+
+# -- IMPORTS --
+import platform, subprocess, os
+import pytesseract, pyperclip, PIL.Image as Image
+from utility import Settings
+
+# Paramètres
+logger = Settings.setup_logging("debugging")
+
+
+# -- FONCTIONS DÉFINIES --
+def select_screenshot():
+    system = platform.system().lower()
+    data_dir_path = os.path.join(os.getcwd(), "data")
+    os.mkdir(current_path, exist_ok=True)
+    current_path = os.path.join(data_dir_path, "image.png")
+    try:
+        # Save to current directory
+        subprocess.run(["screencapture", "-i", current_path], check=True)
+        # Save to clipboard
+            # subprocess.run(["screencapture", "-ic"], check=True)
+        return current_path
+    except Exception as e:
+        logger.error(f"Screenshot : ERROR - {str(e)}")
+        return False
+
+
+def get_text_from_image(image_path):
+    try:
+        # Récupérer l'image depuis le presse-papiers
+        image = Image.open(image_path)
+        # Par défaut en anglais, mais on peut spécifier le français avec lang='fra'
+        text = pytesseract.image_to_string(image, lang='fra')
+        return text.strip()
+    except Exception as e:
+        return f"Une erreur s'est produite : {str(e)}"
+        
+
+def traitement_texte(text, *args):
+    for arg in args:
+        if arg == "--clean" or arg == "-c":
+            text = text.replace("\n", " ")
+            text = text.replace("\t", " ")
+            text = text.replace("  ", " ")
+    return text
+    
+    
+
+def main_tool(*args):
+    logger.info("START")  # Capture_text: main tool: START
+    # - 1 - Prendre une capture d'écran
+    path = select_screenshot()
+    logger.info(f"Screenshot taken: {path}")
+    # - 2 - Extraire le texte
+    text = get_text_from_image(path)
+    logger.info(f"Text extracted: {text}")
+    # - 3 - Traiter le texte si argument(s)
+    if args:
+        text = traitement_texte(text, *args)
+    # - 4 - Copier le texte dans le presse-papiers
+    pyperclip.copy(text)
+    logger.info("Text copied to clipboard")
+    logger.info("END")
+
+
+
